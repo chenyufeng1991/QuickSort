e360e399f332ce4527a9c33c92ab7fff6fce940c
chenyufeng <503434355@qq.com>
2016-02-23 09:40:34 +0800

    1

--- QuickSort/main.c
+++ QuickSort/main.c
@@ -8,68 +8,15 @@
 
 #include <stdio.h>
 
-int *quickSort(int arr[],int l,int r);
 void quickSort02(int *arr,int l,int r);
 
 int main(int argc, const char * argv[]) {
 
     int numArr[5] = {3,6,0,9,4};
-
-    //使用指针返回数组，返回的其实是数组的头指针；
-    /**
-     *  使用返回指针；
-     */
-
-//    int *retArr;
-//    retArr = quickSort(numArr, 0, 4);
-//    for (int i = 0; i < 5; i++) {
-//        //取数组值
-//        printf("%d ",*(retArr + i));
-//    }
-
-
-    /**
-     *  直接传递引用,比较方便；
-     */
     quickSort02(numArr, 0, 4);
     for (int i = 0; i < 5; i++) {
         printf("%d ",numArr[i]);
     }
-}
-
-int *quickSort(int arr[],int l,int r){
-    //当左右指针相等的时候直接返回；
-    if (l < r) {
-        //此时的x就是基准值；
-        int i = l,j = r,x = arr[l];
-
-        //下面的while循环表示一次分治，也就是进行一次排序；
-        while (i < j) {
-            //先从基准值右侧找出小于基准的值；
-            while (i < j && arr[j] >= x) {
-                j--;
-            }
-            if (i < j) {
-                //交换顺序，i++；
-                arr[i++] = arr[j];
-            }
-
-            //从基准值左侧找出大于基准的值；
-            while (i < j && arr[i] < x) {
-                i++;
-            }
-            if (i < j) {
-                //交换顺序，j--;
-                arr[j--] = arr[i];
-            }
-        }
-        //把基准值放入arr[i]位置；
-        arr[i] = x;
-        //递归，左右两侧分别进行快排；
-        quickSort(arr, l, i - 1);
-        quickSort(arr, i + 1, r);
-    }
-    return arr;
 }
 
 void quickSort02(int *arr,int l,int r){
@@ -88,7 +35,6 @@ void quickSort02(int *arr,int l,int r){
                 //交换顺序，i++；
                 arr[i++] = arr[j];
             }
-
             //从基准值左侧找出大于基准的值；
             while (i < j && arr[i] < x) {
                 i++;
@@ -101,11 +47,7 @@ void quickSort02(int *arr,int l,int r){
         //把基准值放入arr[i]位置；
         arr[i] = x;
         //递归，左右两侧分别进行快排；
-        quickSort(arr, l, i - 1);
-        quickSort(arr, i + 1, r);
+        quickSort02(arr, l, i - 1);
+        quickSort02(arr, i + 1, r);
     }
 }
-
-
-
-
